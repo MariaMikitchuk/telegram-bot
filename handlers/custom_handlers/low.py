ef2a609291008ed322a64c8361310d5c5b189450
Maria MikitchuK <6742517@gmail.com>
2024-03-31 16:45:16 +0300

    lines cut, test.py removal, readme.md update

--- handlers/custom_handlers/low.py
+++ handlers/custom_handlers/low.py
@@ -2,7 +2,8 @@ from telebot.types import Message
 
 from api import main_api
 from api.core import url, headers, params, url_dish, params_dish
-from keyboards.reply.low_nutrient_markup import low_reply_nutrient, nutrient_mapping, exit_to_nutrient_markup
+from keyboards.reply.low_nutrient_markup import low_reply_nutrient, nutrient_mapping, \
+    exit_to_nutrient_markup
 from loader import bot
 from states.low_state import LowState
 
@@ -12,12 +13,12 @@ def low_func(message: Message) -> None:
     user_id = message.from_user.id
     chat_id = message.chat.id
     bot.set_state(user_id, LowState.nutrient, chat_id)
-    # TODO Не забывайте сохранять историю команд в БД, для работы с БД рекомендую peewee - будет проще делать запросы,
-    #  надо только немного вникнуть. Либо делать запросы на сыром SQL, если вам это ближе. Не забывайте привязывать
-    #  данные к id пользователя, иначе это будет не многопользовательский бот.
-    bot.send_message(user_id, f'Choose a nutrient, the minimum amount of which should contain a dish in grams\n- '
-                                           f'protein\n- '
-                                           f'carbs\n- calcium\n- phosphorus', reply_markup=low_reply_nutrient())
+    bot.send_message(user_id,
+                     f'Choose a nutrient, the minimum amount of which should contain '
+                     f'a dish in grams\n- '
+                     f'protein\n- '
+                     f'carbs\n- calcium\n- phosphorus',
+                     reply_markup=low_reply_nutrient())
 
 
 @bot.message_handler(state=LowState.nutrient)
@@ -26,11 +27,10 @@ def get_nutrient(message: Message):
     chat_id = message.chat.id
     if message.text.strip().lower() in nutrient_mapping:
         nutrient = message.text.strip().lower()
-        bot.send_message(user_id, 'Thanks, I wrote it down.\nNow enter the amount of it in the dish in grams or press "exit" to choose another '
-                                  'nutrient',
-                         # TODO превышение ограничения на длину строки кода в 120 символов - разбейте строку на две
-                         #  части (и далее по всему проекту поправьте, пожалуйста)
-                         reply_markup=exit_to_nutrient_markup())
+        bot.send_message(user_id,
+                         'Thanks, I wrote it down.\nNow enter the amount of it in the '
+                         'dish in grams or press "exit" to choose another '
+                         'nutrient', reply_markup=exit_to_nutrient_markup())
         bot.set_state(user_id, LowState.value, chat_id)
         with bot.retrieve_data(user_id, chat_id) as data:
             data['nutrient'] = nutrient
@@ -49,15 +49,20 @@ def get_value(message: Message):
         bot.set_state(user_id, LowState.dish, chat_id)
         with bot.retrieve_data(user_id, chat_id) as data:
             data['value'] = message.text
-            response, titles, id_dict, max_dishes = main_api.dish_low_nutrient("GET", url, headers, params, data['value'], data['nutrient'])
+            response, titles, id_dict, max_dishes = main_api.dish_low_nutrient(
+                "GET", url, headers, params, data['value'], data['nutrient']
+            )
             if response:
                 bot.id_dict = id_dict
                 bot.max_dishes = max_dishes
                 bot.send_message(user_id, 'Thanks, here is the list of dishes:\n')
                 bot.send_message(user_id, '\n'.join(titles))
-                bot.send_message(user_id, 'Enter the number of the dish to learn more about it')
+                bot.send_message(user_id,
+                                 'Enter the number of the dish to learn more about it')
             else:
-                bot.send_message(user_id, "Sorry, I couldn't find the dishes according to your request. Enter another quantity:")
+                bot.send_message(user_id,
+                                 "Sorry, I couldn't find the dishes according to your "
+                                 "request. Enter another quantity:")
                 bot.set_state(user_id, LowState.value, chat_id)
     else:
         bot.send_message(message.from_user.id, 'The quantity can be a positive number')
@@ -74,11 +79,12 @@ def get_dish(message: Message):
             data['dish'] = int(message.text)
             dish_id = bot.id_dict.get(data['dish'])
             url_id = url_dish.format(dish_id)
-            summary, link = main_api.dish_low_summary("GET", url_id, headers, params_dish)
+            summary, link = main_api.dish_low_summary("GET", url_id, headers,
+                                                      params_dish)
             bot.send_message(user_id, summary)
-            bot.send_message(user_id, f'Search for the recipe by following the link {link}')
+            bot.send_message(user_id,
+                             f'Search for the recipe by following the link {link}')
             bot.set_state(user_id, None, chat_id)
     else:
-        bot.send_message(user_id, 'Please enter the number of one of the suggested dishes')
-
-
+        bot.send_message(user_id,
+                         'Please enter the number of one of the suggested dishes')
--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -8,12 +8,17 @@ from loader import bot
 @bot.message_handler(commands=["start", "hello-world"])
 def bot_start(message: Message):
     keyboard = create_default_keyboard()
-    bot.reply_to(message, f"Hello, {message.from_user.full_name}!\n Send me 'hello', to continue work", reply_markup=keyboard)
+    bot.reply_to(message,
+                 f"Hello, {message.from_user.full_name}!\n Send me 'hello', "
+                 f"to continue work",
+                 reply_markup=keyboard)
 
 
 @bot.message_handler(func=lambda message: message.text.lower().startswith('hello'))
 def hello_handler(message):
     keyboard = command_markup()
-    bot.send_message(message.from_user.id, 'I am a bot that will help you find recipes for a given nutrient content.\nchoose search parameters:\n- '
-                                           'with the minimum amount(low)\n- with the maximum amount(high)...',
+    bot.send_message(message.from_user.id,
+                     'I am a bot that will help you find recipes for a given nutrient '
+                     'content.\nchoose search parameters:\n- '
+                     'with the minimum amount(low)\n- with the maximum amount(high)...',
                      reply_markup=keyboard)
